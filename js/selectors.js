82a8d19f4daa0259ad2a90ea3c242cb7e1ab1d71
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-03-20 23:43:03 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,6 +8,7 @@ function lastImageSelector()
   return $('img:last');
 }
 
+<<<<<<< HEAD
 
 function divSelector(){
   return $('.pics');
@@ -20,5 +21,17 @@ function ninjaBabySelector(){
 
 function firstListItem()
 {
+<<<<<<< HEAD
   return $('#pic-list li:first-child')
 }
+=======
+  return $('ul first-child:#pic_list')
+}
+=======
+function ninjaBabySelector(){
+  return &('#baby-ninja');
+}
+
+function 
+>>>>>>> 829115629232225b334b9f2f00e0384129f404ba
+>>>>>>> 9b51b0f451d06c716909362c48c37a049bb0114a
